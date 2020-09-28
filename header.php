8eaba987813302a85f0d5f8bc4c38194e74e8ec5
Fazlerabbishoikot <36643554+Fazlerabbishoikot@users.noreply.github.com>
2020-09-28 17:53:58 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<head>
+<title>Store Website</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+
+ });
+
+</script>
+</head>
+<body>
+  <div class="wrap">
+		<div class="header_top">
+			<div class="logo">
+				<a href="index.php"><img src="images/logo.png" alt="" /></a>
+			</div>
+			  <div class="header_top_right">
+			    <div class="search_box">
+				    <form>
+				    	<input type="text" value="Search for Products" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search for Products';}"><input type="submit" value="SEARCH">
+				    </form>
+			    </div>
+			    <div class="shopping_cart">
+					<div class="cart">
+						<a href="#" title="View my shopping cart" rel="nofollow">
+								<span class="cart_title">Cart</span>
+								<span class="no_product">(empty)</span>
+							</a>
+						</div>
+			      </div>
+		   <div class="login"><a href="login.php">Login</a></div>
+		 <div class="clear"></div>
+	 </div>
+	 <div class="clear"></div>
+ </div>
+<div class="menu">
+	<ul id="dc_mega-menu-orange" class="dc_mm-orange">
+	  <li><a href="index.php">Home</a></li>
+	  <li><a href="products.php">Products</a> </li>
+	  <li><a href="topbrands.php">Top Brands</a></li>
+	  <li><a href="cart.php">Cart</a></li>
+	  <li><a href="contact.php">Contact</a> </li>
+	  <div class="clear"></div>
+	</ul>
+</div>
